5b763441be5fbfe138e8cddca8ae5abec58a61ed
asdzxc1062 <asdzxc1062@naver.com>
2023-01-18 09:44:39 +0900

    zxcvzxcv

---
+++ chap06_0113/src/com/yedam/ch06/Car.java
@@ -0,0 +1,29 @@
+package com.yedam.ch06;
+
+public class Car {
+	private int speed;
+	private int maxSpeed;
+	private String model;
+	
+	public Car(String model, int maxSpeed) {
+		this.model = model;
+		this.maxSpeed = maxSpeed;
+		this.speed = 0;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	public int getMaxSpeed() {
+		return this.maxSpeed;
+	}
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public int getSpeed() {
+		return this.speed;
+	}
+	
+}
