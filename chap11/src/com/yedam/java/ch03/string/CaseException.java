d9c62be72f6689db56404f2a6c604762b37d5f50
asdzxc1062 <asdzxc1062@naver.com>
2023-01-18 17:51:31 +0900

    dasdasd

--- chap11/src/com/yedam/java/ch03/string/CaseException.java
+++ chap11/src/com/yedam/java/ch03/string/CaseException.java
@@ -5,14 +5,22 @@ public class CaseException {
 	
 	public static void main(String[] args) {
 		String inputData = "Hello World!";
-		String newInputDataL = inputData.toLowerCase();
-		String newInputDataU = inputData.toUpperCase();
+		String newInputDataL = inputData.toLowerCase(); //소문자
+		String newInputDataU = inputData.toUpperCase(); //대문자
 		
 		if(newInputDataU.equals(DATA.toUpperCase())) {
 			System.out.println("있는 데이터입니다.");
-		} else {
+		}else {
 			System.out.println("없는 데이터입니다.");
 		}
+		// 대소문자 구분없이 의미만 비교하는 메소드
+		if(inputData.equalsIgnoreCase(DATA)) {
+			System.out.println("있는 데이터입니다.");
+		}else {
+			System.out.println("없는 데이터입니다.");
+		}
+		
+		
 	}
 
 }
